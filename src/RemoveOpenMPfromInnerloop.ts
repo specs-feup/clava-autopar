18c2f2f948ddb9d8f1c0156e202ff697a841ad9a
davidscr <up202206312@up.pt>
2025-03-20 18:56:18 +0000

    fixed loops with searchFrom

--- src/RemoveOpenMPfromInnerloop.ts
+++ src/RemoveOpenMPfromInnerloop.ts
@@ -10,36 +10,32 @@ import { Body, Call, FileJp, FunctionJp, Loop, Omp } from "@specs-feup/clava/api
 
 export default function RemoveOpenMPfromInnerloop() {
 
-    for (const $omp of Query.search(FileJp)
-        .search(FunctionJp)
-        .search(Body)
-        .search(Omp, { kind: "parallel for" })) {
-        if (typeof $omp.target !== 'undefined')
-            RemoveSubOmpParallel($omp.target as Loop);
+    for (const $function of Query.search(FileJp).search(FunctionJp)){
+        for (const $omp of Query.searchFrom($function.body, Omp, { kind: "parallel for" })){
+            if (typeof $omp.target !== 'undefined')
+                RemoveSubOmpParallel($omp.target as Loop);
+        }
     }
 
 
     const exclude_func_from_Omp: string[] = [];
 
-    for (const $omp of Query.search(FileJp)
-        .search(FunctionJp)
-        .search(Body)
-        .search(Omp, { kind: "parallel for" })) {
-        if (typeof $omp.target !== 'undefined') {
-            const func_names = find_func_call($omp.target as Loop);
-            for (const func_name of func_names)
-                if (exclude_func_from_Omp.indexOf(func_name) === -1)
-                    exclude_func_from_Omp.push(func_name);
+    for (const $function of Query.search(FileJp).search(FunctionJp)){
+        for (const $omp of Query.searchFrom($function.body, Omp, { kind: "parallel for" })){
+            if (typeof $omp.target !== 'undefined') {
+                const func_names = find_func_call($omp.target as Loop);
+                for (const func_name of func_names)
+                    if (exclude_func_from_Omp.indexOf(func_name) === -1)
+                        exclude_func_from_Omp.push(func_name);
+            }
         }
     }
-
-    for (const $omp of Query.search(FileJp)
-        .search(FunctionJp)
-        .search(Body)
-        .search(Omp, { kind: "parallel for" })) {
-        const func_name = ($omp.getAncestor('function') as FunctionJp).name;
-        if (exclude_func_from_Omp.indexOf(func_name) !== -1)
-            $omp.insert("replace", '// #pragma omp ' + $omp.content + '   remove due to be part of parallel section for function call');
+    for (const $function of Query.search(FileJp).search(FunctionJp)){
+        for (const $omp of Query.searchFrom($function.body, Omp, { kind: "parallel for" })){
+            const func_name = ($omp.getAncestor('function') as FunctionJp).name;
+            if (exclude_func_from_Omp.indexOf(func_name) !== -1)
+                $omp.insert("replace", '// #pragma omp ' + $omp.content + '   remove due to be part of parallel section for function call');
+        }
     }
 }
 /**************************************************************
@@ -48,7 +44,7 @@ export default function RemoveOpenMPfromInnerloop() {
 * 
 **************************************************************/
 function RemoveSubOmpParallel($loop: Loop) {
-    for (const $omp of Query.searchFrom($loop, Body).search(Omp, { kind: "parallel for" })) {
+    for (const $omp of Query.searchFrom($loop.body, Omp, { kind: "parallel for" })) {
         $omp.insert("replace", '// #pragma omp ' + $omp.content);
     }
 }
@@ -56,7 +52,7 @@ function RemoveSubOmpParallel($loop: Loop) {
 function find_func_call($loop: Loop): Array<string> {
     const func_names: string[] = [];
 
-    for (const $call of Query.searchFrom($loop, Body).search(Call, { astName: "CallExpr" })) {
+    for (const $call of Query.searchFrom($loop.body, Call, { astName: "CallExpr" })) {
         func_names.push($call.name);
     }
 
